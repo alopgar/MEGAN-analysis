9882481f1e08b8ed1ec00d08d9e13a9e81c692a0
alopgar <43778995+alopgar@users.noreply.github.com>
2019-07-18 12:46:03 +0200

    Update MinION_MEGAN.sh

--- bin/MinION_MEGAN.sh
+++ bin/MinION_MEGAN.sh
@@ -3,24 +3,28 @@
 
 usage(){
 cat << _EUs_
-$(basename "$0") [OPTIONS]... -- Initiates a pipeline for Nanopore MinION metagenome analysis using DIAMOND + MEGAN.\n
+$(basename "$0") [PATHS]... [OPTIONS]... -- Initiates a pipeline for Nanopore MinION metagenome analysis using DIAMOND + MEGAN.\n
 \nDESCRIPTION:\n
-	\tThis script performs DIAMOND + MEGAN analysis of .fastq files from .fast5 Nanopore MinION data. It launches multiple tasks to remote \n
-	\tserver queue, one per input sample. Modification of input/output paths must be done inside this code, although implementation of new \n
-	\tcommand arguments is being developed. Modifications on default parameters (trimming software, DIAMOND/MEGAN params...) must be done \n
-	\tINSIDE this code (see script MinION_MEGAN_run.sh).\n
+	This script performs DIAMOND + MEGAN analysis of .fastq files from .fast5 Nanopore MinION data. It launches multiple tasks to remote server queue,\n
+	one per input sample. Modifications on default parameters (trimming software, DIAMOND / MEGAN params...) must be done INSIDE this code (see script\n
+	MinION_MEGAN_run.sh).\n
+\nPATHS:\n
+	First mandatory argument -> Input Path. Where .fastq or .fastq.gz files are stored.\n
+	Second mandatory argument -> Output Path. Working directory. Output folders and results will be stored here.\n
 \nPARAMETERS:\n
-	\t-i, --init:\n
-		\t\tThis flag indicates that initial files and configs must be prepared in output dir.\n 
-		\t\tIf NOT SPECIFIED, NO FASTQ SYMLINKS nor ID FILE CREATION will be done.\n
-	\t-F, --final:\n
-		\t\tThis flag enables the use of .log files for creating final stats files:\n
-			\t\t\t-Trimming stats (Prinseq-lite)\n
-			\t\t\t-Running time for DIAMOND files\n
-			\t\t\t-Running time for MEGAN files\n
-		\t\tIf this flag is indicated, DIAMOND + MEGAN processing will NOT be done.\n
-	\t-h, --help:\n
-		\t\tPrints this screen.\n
+	-i, --init:\n
+	\tThis flag indicates that initial files and configs must be prepared in output dir. If NOT SPECIFIED, NO FASTQ SYMLINKS nor ID FILE CREATION\n
+	\twill be done.\n
+	-F, --final:\n
+	\tThis flag enables the use of .log files for creating final stats files:\n
+	\t\t-Trimming stats (Prinseq-lite)\n
+	\t\t-Running time for DIAMOND files\n
+	\t\t-Running time for MEGAN files\n
+	\tIf this flag is indicated, DIAMOND + MEGAN processing will NOT be done.\n
+	-t, --tempdir:\n
+	\tCustomize temp directory where input data will be copied and unzipped.If NOT SPECIFIED, input data will be copied in [Input_path]/temp.\n 
+	-h, --help:\n
+	\tPrints this screen.\n
 _EUs_
 }
 
@@ -34,21 +38,23 @@ export DMND=$LUSTRE/NCBI_db/nr.dmnd
 export MEGAN_a2t=$LUSTRE/NCBI_db/prot_acc2tax-May2017.abin
 export MEGAN_ip2g=$LUSTRE/NCBI_db/acc2interpro-Nov2016XX.abin
 
-export PTH=/mnt/netapp2/Store_uni/home/otras/ini/ogr/SHARED_DATA/METALGENmetagenomas
-export PTH_data=$STORE/Data/FULL_METAGENOME/METALGEN_MINION
-export WDIR=$LUSTRE/Results/Metagenome/METALGEN
+export PTH=$1
+export PTH_data=$PTH/temp
+export WDIR=$2; shift 2
 ini=
 final=0
 
-OPTS=`getopt -o i::F::h --long init::,final::,help -- "$@"`
+OPTS=`getopt -o iFt:h --long init,final,tempdir:,help -- "$@"`
 eval set -- "$OPTS"
 
 while true; do
 	case $1 in
 		-i | --init)
-			ini=1; shift 2 ;;
+			ini=1; shift ;;
 		-F | --final)
-			final=1; shift 2 ;;
+			final=1; shift ;;
+		-t | --tempdir)
+			export PTH_data=$2; shift 2 ;;
 		-h | --help)
 			echo -e $(usage) | less ; exit ;;
 		--) shift ; break ;;
@@ -56,8 +62,11 @@ while true; do
 	esac
 done
 
-if [ ! -d $PTH_data ];then mkdir -p $PTH_data; fi
 if [ ! -d $WDIR/1_Rawdata ];then mkdir -p $WDIR/1_Rawdata; fi
+
+echo "------ INPUT DIRECTORY: "$PTH
+echo "------ TEMP DIRECTORY: "$PTH_data
+echo "------ OUTPUT DIRECTORY: "$WDIR
 
 ####################################################################
 # RUNNING SCRIPT
@@ -68,15 +77,21 @@ export IDfile=$WDIR/INP_files.txt
 
 if [ "$ini" == 1 ]; then
 	echo "INI"
+	if [ ! -d $PTH_data ];then mkdir -p $PTH_data; fi
 	if [ -f $WDIR/INP_files_all.txt ]; then mv $WDIR/INP_files_all.txt $WDIR/INP_files_done.txt
 	else touch $WDIR/INP_files_done.txt; fi
 	ls $PTH/*fastq* | xargs -n1 basename | awk '{gsub(".fastq",""); gsub(".gz",""); print }' > $WDIR/INP_files_all.txt
 	diff $WDIR/INP_files_all.txt $WDIR/INP_files_done.txt | grep '<' | sed 's/<\ //' > $WDIR/INP_files.txt
 	export Input=$(awk '{ print $1 }' $IDfile)
 	for f in $Input; do
+		echo "Copying file "$f" from INPUT DIR to TEMP DIR"
 		cp $PTH/$f* $PTH_data
-		if [ -f $PTH_data/$f.fastq.gz ]; then gunzip $PTH_data/$f.fastq.gz; fi
+		if [ -f $PTH_data/$f.fastq.gz ]; 
+			echo "File .gz --- gunzipping..."
+			then gunzip $PTH_data/$f.fastq.gz
+		fi
 		ln -s $PTH_data/$f.fastq $WDIR/1_Rawdata
+		echo "Copying done --- Symlink created in 1_Rawdata"
 	done
 else export Input=$(awk '{ print $1 }' $IDfile); fi
 
@@ -84,14 +99,19 @@ else export Input=$(awk '{ print $1 }' $IDfile); fi
 if [ ! -d $WDIR/1_Rawdata/trim_bad ];then mkdir $WDIR/1_Rawdata/trim_bad; fi
 if [ ! -d $WDIR/1_Rawdata/trim_logs ];then mkdir $WDIR/1_Rawdata/trim_logs; fi
 
+## DIAMOND + MEGAN FOLDERS:
+if [ ! -d $WDIR/2_files.daa ];then mkdir $WDIR/2_files.daa; fi
+if [ ! -d $WDIR/3_files.rma ];then mkdir $WDIR/3_files.rma; fi
+if [ ! -d $WDIR/4_files.rmainfo ];then mkdir $WDIR/4_files.rmainfo; fi
+
 ## EXE:
 if [ "$final" == 0 ]; then
 	for file in $Input; do
-		sbatch MinION_MEGAN_run.sh $file
+		sbatch ./bin/MinION_MEGAN/MinION_MEGAN_run.sh $file
 	done
 fi
 
 ## FINALSTATS:
 if [ "$final" == 1 ]; then
-	sbatch MinION_MEGAN_finalstats.sh
+	sh ./bin/MinION_MEGAN/MinION_MEGAN_finalstats.sh
 fi
